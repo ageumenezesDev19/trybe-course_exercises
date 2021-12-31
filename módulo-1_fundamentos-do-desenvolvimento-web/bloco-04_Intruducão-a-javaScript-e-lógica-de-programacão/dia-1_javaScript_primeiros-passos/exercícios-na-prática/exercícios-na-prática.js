c7ce751a5e94f69a3ff6ddcd97b22264b11fc910
ageu_dev19 <ageumenezes423@gmail.com>
2021-12-31 16:07:17 -0300

    Fix a bug in the exercise 4.1

--- módulo-1_fundamentos-do-desenvolvimento-web/bloco-04_Intruducão-a-javaScript-e-lógica-de-programacão/dia-1_javaScript_primeiros-passos/exercícios-na-prática/exercícios-na-prática.js
+++ módulo-1_fundamentos-do-desenvolvimento-web/bloco-04_Intruducão-a-javaScript-e-lógica-de-programacão/dia-1_javaScript_primeiros-passos/exercícios-na-prática/exercícios-na-prática.js
@@ -142,7 +142,7 @@ let lucro = valor - (custo + (custo * (20 / 100)));
 console.log("Lucro = R$" + lucro);
 
 //11
-let salarioBruto = 90000;
+let salarioBruto = 3000;
 let inss;
 let ir;
 let alicota;
@@ -150,31 +150,41 @@ let alicota;
 if (salarioBruto <= 1556.94 && salarioBruto > 0) {
     alicota = (8 / 100);
     inss = salarioBruto * alicota;
+    console.log ("INSS = R$" + inss);
 } else if (salarioBruto >= 1556.95 && salarioBruto <= 2594.92) {
     alicota = (9 / 100);
     inss = salarioBruto * alicota;
+    console.log ("INSS = R$" + inss);
 } else if (salarioBruto >= 2594.93 && salarioBruto <= 5189.82) {
     alicota = (11 / 100)
     inss = salarioBruto * alicota;
+    console.log ("INSS = R$" + inss);
 } else if (salarioBruto > 5189.82) {
     alicota = 570.88;
     inss = alicota;
+    console.log ("INSS = R$" + inss);
 } else {
     console.log("ERROR! Invalid data!")
 }
 
 let salarioBase = salarioBruto - inss; 
+console.log("Salário base = R$" + salarioBase);
 
-if (salarioBase <= 1903,98) {
+if (salarioBase <= 1903.98) {
     ir = 0;
+    console.log("IR = R$" + ir);
 } else if (salarioBase >= 1903.99 && salarioBase <= 2826.65) {
     ir = (salarioBase * (7.5 / 100)) - 142.80;
+    console.log("IR = R$" + ir);
 } else if (salarioBase >= 2826.66 && salarioBase <= 3751.05) {
     ir = (salarioBase * (15 / 100)) - 354.80;
+    console.log("IR = R$" + ir);
 } else if (salarioBase >= 3751.06 && salarioBase <= 4664.68) {
     ir = (salarioBase * (22.5 / 100)) - 636.13;
+    console.log("IR = R$" + ir);
 } else if (salarioBase > 4664.68) {
     ir = (salarioBase * (27.5 / 100)) - 869.36;
+    console.log("IR = R$" + ir);
 }
 
 let salarioLiquido = salarioBase - ir;
