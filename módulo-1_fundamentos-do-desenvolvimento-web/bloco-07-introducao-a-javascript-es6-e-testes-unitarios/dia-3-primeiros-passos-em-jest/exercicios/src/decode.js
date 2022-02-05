e779bc3d09853a719ef7a43ee2bdf73145a11c6b
ageu_dev19 <ageumenezes423@gmail.com>
2022-02-05 08:13:38 -0300

    Add function test - encode.test.js and decode.test.js

--- módulo-1_fundamentos-do-desenvolvimento-web/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/exercicios/src/decode.js
+++ módulo-1_fundamentos-do-desenvolvimento-web/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/exercicios/src/decode.js
@@ -1,4 +1,24 @@
-function decode();
+function decode(string) {
+    // seu código aqui
+    let decode = 1;
+    for (let i = 0; i < string.length; i += 1) {
+      if (string[i] === "1") {
+        decode += "a";
+      } else if (string[i] === "2") {
+        decode += "e";
+      } else if (string[i] === "3") {
+        decode += "i";
+      } else if (string[i] === "4") {
+        decode += "o";
+      } else if (string[i] === "5") {
+        decode += "u";
+      } else {
+        decode += string[i];
+      }
+    }
 
+    decode = decode.substring(1, decode.length);
+    return decode;
+}
 
 module.exports = decode;
