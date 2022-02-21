09340395317f3999ceb6d8f6d3b4cf7a82491baa
ageu_dev19 <ageumenezes423@gmail.com>
2022-02-21 07:43:54 -0300

    Add block 7.3

---
+++ módulo-1_fundamentos-do-desenvolvimento-web/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/exercicios/src/encode.js
@@ -0,0 +1,27 @@
+const encode = (string) => {
+    let theEncode = "1";
+    if (string != null) {
+        for (let i = 0; i < string.length; i += 1) {
+            if (string[i] === "a") {
+              theEncode += "1";
+            } else if (string[i] === "e") {
+              theEncode += "2";
+            } else if (string[i] === "i") {
+              theEncode += "3";
+            } else if (string[i] === "o") {
+              theEncode += "4";
+            } else if (string[i] === "u") {
+              theEncode += "5";
+            } else {
+              theEncode += string[i];
+            }
+          }
+      
+          theEncode = theEncode.substring(1, theEncode.length);
+          return theEncode;
+    }
+
+  // -> substring(startIndex, endIndex) -> fonte: https://www.delftstack.com/pt/howto/javascript/javascript-remove-first-character-from-string/
+}
+
+module.exports = encode;
