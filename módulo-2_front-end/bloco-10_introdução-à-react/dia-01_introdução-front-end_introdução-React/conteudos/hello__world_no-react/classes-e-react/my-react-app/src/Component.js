faf57a6078e5e312c707762e0ee5507a5d06ce46
ageu_dev19 <ageumenezes423@gmail.com>
2022-04-08 18:18:48 -0300

    Update the contents

---
+++ módulo-2_front-end/bloco-10_introdução-à-react/dia-01_introdução-front-end_introdução-React/conteudos/hello__world_no-react/classes-e-react/my-react-app/src/Component.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+class MyComponent extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Ageu Menezes Costa</h1>
+                <p>Olá, sou estudante de desenvolvimento Web na Trybe<br></br>
+                    e estou iniciando o módulo de Front-end<br></br>
+                    aprendendo React.<br></br><br></br>
+                    Este é o primeiro componente React de classe que criei!!</p>
+            </div>
+        )
+    }
+}
+
+export default MyComponent;
